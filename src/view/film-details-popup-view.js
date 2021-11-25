e86aa69302336dd978bd1b36b7b83a2094450492
Aliya <weliant13@gmail.com>
2021-11-26 01:53:02 +0500

    Разбиение верстки на отдельные компоненты

---
+++ src/view/film-details-popup-view.js
@@ -0,0 +1,31 @@
+export const createFilmDetailsPopupTemplate = () => (
+  `<section class="film-details">
+    <form class="film-details__inner" action="" method="get">
+      <div class="film-details__top-container">
+        <div class="film-details__close">
+          <button class="film-details__close-btn" type="button">close</button>
+        </div>
+        <div class="film-details__info-wrap">
+
+        </div>
+
+        <section class="film-details__controls">
+          <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
+          <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
+          <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
+        </section>
+      </div>
+
+      <div class="film-details__bottom-container">
+        <section class="film-details__comments-wrap">
+          <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
+          <ul class="film-details__comments-list">
+          </ul>
+          <div class="film-details__new-comment">
+          </div>
+        </section>
+      </div>
+    </form>
+  </section>
+  `
+);
